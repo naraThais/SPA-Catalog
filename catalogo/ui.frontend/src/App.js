53dd05949fa626723dd51f10729ca5d607ea8840
Thais Nara <thaisna46@gmail.com>
2025-07-28 17:08:37 -0300

    Criando projeto catalogo com spa aem

---
+++ catalogo/ui.frontend/src/App.js
@@ -0,0 +1,18 @@
+import { Page, withModel } from '@adobe/aem-react-editable-components';
+import React from 'react';
+
+// This component is the application entry point
+class App extends Page {
+  render() {
+    return (
+      <div>
+        {this.childComponents}
+        {this.childPages}
+
+        <p>OIIIIIIIIIII TESTE</p>
+      </div>
+    );
+  }
+}
+
+export default withModel(App);
